920fbde777defa26aa96c78b044d5f3110bf763c
Thomas Faughnan <tom@tjf.sh>
2021-12-02 11:34:36 -0500

    condition bodies on same line

--- day02/1.py
+++ day02/1.py
@@ -1,10 +1,7 @@
 import sys;x=y=0
 for l in sys.stdin:
  r,M=l.split();m=int(M)
- if r[0]<'f':
-  y+=m
- elif r[0]<'u':
-  x+=m
- else:
-  y-=m
+ if r[0]<'f':y+=m
+ elif r[0]<'u':x+=m
+ else:y-=m
 print(x*y)
--- day02/2.py
+++ day02/2.py
@@ -1,10 +1,7 @@
 import sys;x=y=a=0
 for l in sys.stdin:
  r,M=l.split();m=int(M)
- if r[0]<'f':
-  a+=m
- elif r[0]<'u':
-  x+=m;y+=a*m
- else:
-  a-=m
+ if r[0]<'f':a+=m
+ elif r[0]<'u':x+=m;y+=a*m
+ else:a-=m
 print(x*y)
