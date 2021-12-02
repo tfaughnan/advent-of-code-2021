f68be475b9ca5cee6264d54d7e9d846c21a3b84c
Thomas Faughnan <tom@tjf.sh>
2021-12-02 01:26:11 -0500

    shaved off 2 bytes in each part

--- day02/1.py
+++ day02/1.py
@@ -1,10 +1,10 @@
 import sys;x=y=0
 for l in sys.stdin:
  r,M=l.split();m=int(M)
- if r[0]=='f':
-  x+=m
- elif r[0]=='d':
+ if r[0]<'f':
   y+=m
+ elif r[0]<'u':
+  x+=m
  else:
   y-=m
 print(x*y)
