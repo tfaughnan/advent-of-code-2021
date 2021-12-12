7669b47c9d015f2049d7fa94aeb574fdc49c02ce
Thomas Faughnan <tom@tjf.sh>
2021-12-12 04:42:26 -0500

    golfed day11

---
+++ day11/1.py
@@ -0,0 +1,15 @@
+import sys
+from collections import defaultdict as d
+r=*range(10),
+I=[[*map(int,[*l.strip()])]for l in open(0)]
+C=d(lambda:-sys.maxsize,{(x,y):I[y][x]for x in r for y in r})
+def g(c):
+ f=[];[c.update({(x,y):c[(x,y)]+1})or(f.append((x,y))if c[(x,y)]==10 else 0)for y in r for x in r];s=0
+ while len(f)>0:
+  s+=len(f);F=[]
+  for x,y in f:
+   for i,j in((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)):
+    c.update({(x+i,y+j):c[(x+i,y+j)]+1})or(F.append((x+i,y+j))if c[(x+i,y+j)]==10 else 0)
+  f=F
+ [c.update({(x,y):0})for y in r for x in r if c[(x,y)]>9];return s
+print(sum([g(C)for i in range(100)]))
